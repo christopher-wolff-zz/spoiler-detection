a8e1acc6a1109aea1800a0ea098fbb08d0d0546c
Christopher Wolff <christopher.wolff@duke.edu>
2018-03-18 18:40:01 -0400

    .

--- scraper.py
+++ scraper.py
@@ -3,16 +3,18 @@ from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 
-num_titles = 254417  # in 2017
-titles_per_page = 50
-num_pages = math.ceil(num_titles / titles_per_page)
+years = range(2008, 2018)
+movies_per_year = 100
+movies_per_page = 50
+num_pages = math.ceil(movies_per_year / movies_per_page)
 
-for k in range(10):
-    print(f'Page {k + 1}')
-    url = 'http://www.imdb.com/search/title'
-    payload = {'release_date': 2017, 'sort': 'user_rating,desc', 'view': 'advanced',
-               'page': k + 1, 'ref_': 'adv_prv'}
-    response = requests.get(url, params=payload)
-
-    html_soup = BeautifulSoup(response.text, 'html.parser')
-    pprint(html_soup)
+movies = []
+for year in years:
+    for page in range(1, num_pages + 1):
+        print('Year %d, Page %d' % (year, page))
+        url = 'http://www.imdb.com/search/title'
+        payload = {'year': year, 'sort': 'num_votes,desc',
+                   'view': 'advanced', 'page': page, 'ref_': 'adv_prv',
+                   'languages': 'en'}
+        response = requests.get(url, params=payload)
+        soup = BeautifulSoup(response.text, 'html.parser')
