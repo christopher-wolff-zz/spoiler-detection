48706fe259157dcf9176c0cc121b2533800afea5
Christopher Wolff <christopher.wolff@duke.edu>
2018-03-18 19:16:04 -0400

    Clean up more

--- scraper.py
+++ scraper.py
@@ -2,20 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 years = range(2008, 2018)
-num_movies = 100
+movies_per_year = 100
 
-movies = []
+movies = list()
 for year in years:
     print('Year %d' % year)
     url = 'http://www.imdb.com/search/title'
-    payload = {'year': year, 'sort': 'num_votes,desc', 'count': num_movies,
+    payload = {'year': year, 'sort': 'num_votes,desc', 'count': movies_per_year,
                'view': 'advanced', 'ref_': 'adv_prv', 'languages': 'en',
                'title_type': 'feature'}
     response = requests.get(url, params=payload)
     soup = BeautifulSoup(response.text, 'html.parser')
     movie_divs = soup.find_all('div', class_='lister-item-content')
     for movie_div in movie_divs:
-        movie = {}
+        movie = dict()
         movie['name'] = movie_div.h3.a.text
         movie['year'] = year
         movie['rating'] = float(movie_div.strong.text)
