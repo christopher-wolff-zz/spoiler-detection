08524e7768f5ace9ae48f4ee9d4cdf06a30a0676
Christopher Wolff <christopher.wolff@duke.edu>
2018-03-19 18:46:12 -0400

    we did it

--- web_scraper.py
+++ web_scraper.py
@@ -4,35 +4,32 @@ import requests
 
 from bs4 import BeautifulSoup
 
-years = range(2008, 2018)
-movies_per_year = 100
 
 base_url = 'http://www.imdb.com'
 
 
-def scrape_movies() -> list:
+def scrape_movies(num_movies, year_start, year_end) -> list:
     """..."""
     movies = list()
-    for year in years:
+    for year in range(year_start, year_end + 1):
         print('Year: %d' % year)
         movie_url = base_url + '/search/title'
         movie_params = {'year': year, 'sort': 'num_votes,desc', 'ref_': 'adv_prv',
-                        'count': movies_per_year, 'view': 'advanced',
+                        'count': num_movies, 'view': 'advanced',
                         'languages': 'en', 'title_type': 'feature'}
         movie_response = requests.get(movie_url, params=movie_params)
         print('Movie URL: ' + movie_response.url)
         movie_soup = BeautifulSoup(movie_response.text, 'html.parser')
         for k, movie_div in enumerate(movie_soup.find_all('div', class_='lister-item-content')):
             movie = dict()
+            url_temp_1 = movie_div.h3.a['href']  # /title/tt...
+            url_temp_2 = url_temp_1[9:]  # id starts at 9th character
+            movie['id'] = url_temp_2[:url_temp_2.find('/')]
             movie['name'] = movie_div.h3.a.text
             movie['year'] = year
             movie['rating'] = float(movie_div.strong.text)
-            movie['votes'] = movie_div.find('span', attrs={'name': 'nv'})['data-value']
-            movie['url'] = base_url + movie_div.h3.a['href']
-            id_index_start = movie['url'].find('title') + 8
-            url_temp = movie['url'][id_index_start:]
-            id_index_end = url_temp.find('/')
-            movie['id'] = url_temp[:id_index_end]
+            movie['votes'] = int(movie_div.find('span', attrs={'name': 'nv'})['data-value'])
+            movie['url'] = base_url + url_temp_1
             movies.append(movie)
             if (k + 1) % 10 == 0:
                 print('Finished scraping %d movies' % (k + 1))
@@ -55,10 +52,11 @@ def scrape_reviews(movies) -> list:
             rating = review_div.find('span', class_='rating-other-user-rating')
             if not rating:
                 continue
-            review['rating'] = int(rating.find('span').text)
             review['title'] = review_div.find('div', class_='title').text
             review['date'] = review_div.find('span', class_='review-date').text
-            review['text'] = review_div.find('div', class_=["text show-more__control", "text show-more__control clickable"])
+            review['rating'] = int(rating.find('span').text)
+            review['text'] = review_div.find('div', class_=["text show-more__control", "text show-more__control clickable"]).text
+            review['url'] = review_url
             review['movie_id'] = movie['id']
             reviews.append(review)
     return reviews
@@ -67,7 +65,7 @@ def scrape_reviews(movies) -> list:
 def export_to_csv(obj, file_name):
     """Export a list of dictionaries to a csv file."""
     keys = obj[0].keys()
-    with open(file_name, 'wb') as output_file:
+    with open(file_name, 'w') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(obj)
@@ -75,15 +73,15 @@ def export_to_csv(obj, file_name):
 
 def export_to_json(obj, file_name):
     """Export a list of dictionaries to a json file."""
-    with open(file_name, 'wb') as output_file:
+    with open(file_name, 'w') as output_file:
         json.dump(obj, output_file)
 
 
 if __name__ == '__main__':
-    movies = scrape_movies()
+    movies = scrape_movies(100, 2008, 2017)
     reviews = scrape_reviews(movies)
     print('Found %d movies and %d reviews' % (len(movies), len(reviews)))
-    export_to_csv(movies, 'movies.csv')
-    export_to_csv(reviews, 'reviews.csv')
-    export_to_json(movies, 'movies.json')
-    export_to_json(reviews, 'reviews.json')
+    export_to_csv(movies, 'movies_raw.csv')
+    export_to_csv(reviews, 'reviews_raw.csv')
+    export_to_json(movies, 'movies_raw.json')
+    export_to_json(reviews, 'reviews_raw.json')
