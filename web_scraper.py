0a9120b94ad863274161a02991dbf8d3b15e6d79
Christopher Wolff <christopher.wolff@duke.edu>
2018-03-19 20:20:15 -0400

    asdf

--- web_scraper.py
+++ web_scraper.py
@@ -8,54 +8,93 @@ from bs4 import BeautifulSoup
 base_url = 'http://www.imdb.com'
 
 
-def scrape_movies(num_movies, year_start, year_end) -> list:
-    """..."""
+def scrape_movies(years, num_movies=100, debug=False) -> list:
+    """Scrape the most popular movies from the specified years from IMDB.
+
+    The method collects the unique identification number, name, year, average
+    rating, total number of votes, and url for each movie. Only English movies
+    that were categorized as feature films are considered.
+
+    Args:
+    =====
+    years (list): The years to be considered
+    num_movies (int): The maximum number of movies to be collected for each year
+    debug (bool): Whether to display debug messages
+
+    Returns:
+    ========
+    A list of dictionary objects representing the movies
+    """
     movies = list()
-    for year in range(year_start, year_end + 1):
-        print('Year: %d' % year)
-        movie_url = base_url + '/search/title'
-        movie_params = {'year': year, 'sort': 'num_votes,desc', 'ref_': 'adv_prv',
-                        'count': num_movies, 'view': 'advanced',
-                        'languages': 'en', 'title_type': 'feature'}
-        movie_response = requests.get(movie_url, params=movie_params)
-        print('Movie URL: ' + movie_response.url)
-        movie_soup = BeautifulSoup(movie_response.text, 'html.parser')
+    for year in years:
+        if debug:
+            print('Year: %d' % year)
+        url = base_url + '/search/title'
+        payload = {'year': year, 'languages': 'en', 'ref_': 'adv_prv',
+                   'count': num_movies, 'sort': 'num_votes,desc',
+                   'title_type': 'feature', 'view': 'advanced'}
+        response = requests.get(url, params=payload)
+        if debug:
+            print('Movie URL: ' + response.url)
+        movie_soup = BeautifulSoup(response.text, 'html.parser')
         for k, movie_div in enumerate(movie_soup.find_all('div', class_='lister-item-content')):
             movie = dict()
-            url_temp_1 = movie_div.h3.a['href']  # /title/tt...
-            url_temp_2 = url_temp_1[9:]  # id starts at 9th character
-            movie['id'] = url_temp_2[:url_temp_2.find('/')]
+            temp_1 = movie_div.h3.a['href']  # /title/tt...
+            temp_2 = temp_1[9:]  # id starts at 9th character
+            movie['id'] = temp_2[:temp_2.find('/')]
             movie['name'] = movie_div.h3.a.text
             movie['year'] = year
-            movie['rating'] = float(movie_div.strong.text)
-            movie['votes'] = int(movie_div.find('span', attrs={'name': 'nv'})['data-value'])
-            movie['url'] = base_url + url_temp_1
+            movie['avg_rating'] = float(movie_div.strong.text)
+            movie['num_votes'] = int(movie_div.find('span', attrs={'name': 'nv'})['data-value'])
+            movie['url'] = base_url + temp_1
             movies.append(movie)
-            if (k + 1) % 10 == 0:
+            if debug and (k + 1) % 10 == 0:
                 print('Finished scraping %d movies' % (k + 1))
     return movies
 
 
-def scrape_reviews(movies) -> list:
-    """..."""
+def scrape_reviews(movies, debug=False) -> list:
+    """Scrape the most helpful reviews for the given movies from IMDB.
+
+    The method collects the 25 most helpful reviews for each movie, but discards
+    those that do not have a rating. It collects the title, date, rating, full
+    text, number of people that found the review helpful, total number of people
+    who voted on the helpfulness of the review, url, and movie id of the movie
+    that the review was for.
+
+    Args:
+    =====
+    movies (list): The movies of interest
+    debug (bool): Whether to display debug messages
+
+    Returns:
+    ========
+    A list of review objects representing the reviews
+    """
     reviews = list()
     for k, movie in enumerate(movies):
-        print('Movie %d: ' % (k + 1), end='')
+        if debug:
+            print('Movie %d: ' % (k + 1), end='')
         question_mark_index = movie['url'].find('?')
         review_url = movie['url'][:question_mark_index] + 'reviews'
         review_params = {'sort': 'helpfulnessScore', 'dir': 'desc'}
         review_response = requests.get(review_url, params=review_params)
-        print(review_response.url)
+        if debug:
+            print(review_response.url)
         review_soup = BeautifulSoup(review_response.text, 'html.parser')
         for review_div in review_soup.find_all('div', class_='lister-item-content'):
             review = dict()
             rating = review_div.find('span', class_='rating-other-user-rating')
             if not rating:
                 continue
+            helpfulness = review_div.find('div', class_='actions text-muted').text
+            tokens = helpfulness.strip().split()
             review['title'] = review_div.find('div', class_='title').text
             review['date'] = review_div.find('span', class_='review-date').text
             review['rating'] = int(rating.find('span').text)
             review['text'] = review_div.find('div', class_=["text show-more__control", "text show-more__control clickable"]).text
+            review['num_helpful_yes'] = tokens[0]
+            review['num_helpful_total'] = tokens[3]
             review['url'] = review_url
             review['movie_id'] = movie['id']
             reviews.append(review)
@@ -63,7 +102,13 @@ def scrape_reviews(movies) -> list:
 
 
 def export_to_csv(obj, file_name):
-    """Export a list of dictionaries to a csv file."""
+    """Export a list of dictionaries to a csv file.
+
+    Args:
+    =====
+    obj (list): A list of dictionaries representing the object to be exported
+    file_name (str): The destination file name
+    """
     keys = obj[0].keys()
     with open(file_name, 'w') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
@@ -72,14 +117,20 @@ def export_to_csv(obj, file_name):
 
 
 def export_to_json(obj, file_name):
-    """Export a list of dictionaries to a json file."""
+    """Export a list of dictionaries to a json file.
+
+    Args:
+    =====
+    obj (list): A list of dictionaries representing the object to be exported
+    file_name (str): The destination file name
+    """
     with open(file_name, 'w') as output_file:
         json.dump(obj, output_file)
 
 
 if __name__ == '__main__':
-    movies = scrape_movies(100, 2008, 2017)
-    reviews = scrape_reviews(movies)
+    movies = scrape_movies(years=range(2008, 2018), num_movies=100, debug=True)
+    reviews = scrape_reviews(movies, debug=True)
     print('Found %d movies and %d reviews' % (len(movies), len(reviews)))
     export_to_csv(movies, 'movies_raw.csv')
     export_to_csv(reviews, 'reviews_raw.csv')
